f66b09c2d20c2c01882ccc1a989b4be9309dabe4
prajyotgorlewar <prajyotgorlewar@gmail.com>
2025-07-07 12:03:51 +0530

    Docs: Updated strengths.txt with more details
    This commit updates the strengths.txt file to provide a more detailed overview of my background, including:
    
    - Added information about my family and current internship.
    - Elaborated on my role in the CampusNav project.
    - Minor grammatical fixes and improvements to the intro and STAR section.

---
+++ MERN/Project/data.js
@@ -0,0 +1,11 @@
+// var = function hoisted
+// 'Let' being a bloack scope we can minimise the memory usage
+// Conrtol statement always lead to decision making and looping leads to autmoation
+// else if come in to picture when we check related values
+// When we check for username and password its is only good to check the username first.
+
+
+
+let age = 25;
+let status = age >= 18 ? 'You are an adult' : 'You are not an adult';
+console.log(status);
---
+++ MERN/playground-2.mongodb.js
@@ -0,0 +1,48 @@
+// db.createCollection("employees");
+
+// db.employees.insertMany([
+//     { eid: 102, ename: "pajyot", department: "HR1" },
+//     { eid: 103, ename: "prajyot", department: "HR2" },
+//     { eid: 104, ename: "prjyot", department: "HR3" }
+// ]);
+
+// db.employees.find({});
+// db.employees.find({ ename: "prajyot" });
+
+
+// Create collection
+// db.createCollection("department");
+
+// Insert data
+// db.department.insertMany([
+//     { did: 100, dname: "HR" },
+//     { did: 101, dname: "Sales" },
+//     { did: 102, dname: "Payroll" },
+//     { did: 103, dname: "Accounts" },
+// ])
+
+// // Display all data
+// db.department.find({});
+
+// // Find a record where dname is "Sales"
+// db.department.find({ dname: "Sales" });
+
+
+db.employees.insertOne({
+    name: "Prajyot",
+    age: 22,
+    isActive: true,
+    skills: ["JS", "HTML", "CSS", "React", "Node", "MongoDB"],
+    address: {
+        city: "Nagpur",
+        pincode: 440013
+    },
+    joinedOn: new Date("2025-01-01")
+});
+
+db.employees.find({});
+
+db.employees.find({ "address.city": "Nagpur" });
+db.employees.find({ "skills.0": "JS" }, { "address.city": "Nagpur" });
+
+
